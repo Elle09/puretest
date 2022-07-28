3b4531cc01c4f8a1361907b1290caaecb326f28d
Elle09 <81345563+Elle09@users.noreply.github.com>
2022-07-28 12:47:03 +0530

    Add files via upload

---
+++ Estore-nodejs-sqlite/util/database.js
@@ -0,0 +1,8 @@
+const sqlite3 = require("sqlite3");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./myDb.sqlite3",
+});
+
+module.exports = sequelize;
